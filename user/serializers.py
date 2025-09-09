6f6fb650751875d4cc31d96ab65e18de33913f90
Vasyl Boliukh <vskesha@gmail.com>
2025-09-09 18:25:33 +0300

    updated user serializer

--- user/serializers.py
+++ user/serializers.py
@@ -2,12 +2,14 @@ from django.contrib.auth import get_user_model, authenticate
 from django.utils.translation import gettext as _
 from rest_framework import serializers
 
+User = get_user_model()
+
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
-        model = get_user_model()
+        model = User
         fields = ("id", "email", "password", "is_staff")
-        read_only_fields = ("is_staff",)
+        read_only_fields = ("id", "is_staff")
         extra_kwargs = {
             "password": {
                 "write_only": True,
@@ -19,17 +21,23 @@ class UserSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         """Create a new user with encrypted password and return it"""
-        return get_user_model().objects.create_user(**validated_data)
+        return User.objects.create_user(**validated_data)
 
     def update(self, instance, validated_data):
         """Update a user, set the password correctly and return it"""
         password = validated_data.pop("password", None)
-        user = super().update(instance, validated_data)
+        for key in ("username", "email"):
+            setattr(
+                instance,
+                key,
+                validated_data.get(key, getattr(instance, key))
+            )
         if password:
-            user.set_password(password)
-            user.save()
+            instance.set_password(password)
 
-        return user
+        instance.save()
+
+        return instance
 
 
 class AuthTokenSerializer(serializers.Serializer):
